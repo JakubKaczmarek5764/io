0b9497f4f60aa06d17c05c4f2a1bdadbacc895b8
247686 Kaczmarek Jakub <247686@edu.p.lodz.pl>
2024-12-10 22:07:13 +0100

    testy z nullem zamiast donatora

--- src/test/java/DonationsRepositoryTest.java
+++ src/test/java/DonationsRepositoryTest.java
@@ -1,11 +1,5 @@
 import Classes.Donation;
-import Classes.Donator;
-import Classes.Charity;
-import db.CharityRepository;
 import db.DonationsRepository;
-import db.ReportRepository;
-import db.UsersRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,34 +11,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DonationsRepositoryTest {
     private DonationsRepository donationsRepository = new DonationsRepository();
-    private CharityRepository charityRepository = new CharityRepository();
-    private UsersRepository usersRepository = new UsersRepository();
-    private Charity charity = new Charity("zubr", "organizacja pomocowa od 1978 roku w Polsce");
-    private Donator donator = new Donator("Piotr", "Nowak", charity);
 
 
     @BeforeEach
     void cleanUp() {
         donationsRepository.getAll().forEach(donation -> donationsRepository.remove(donation.getDonation_id()));
-        charityRepository.getAll().forEach(charity -> charityRepository.remove(charity.getCharity_id()));
-        usersRepository.getAll().forEach(user -> usersRepository.remove(user.getUserId()));
-        charityRepository.add(charity);
-        usersRepository.add(donator);
     }
     @Test
     public void addTest() {
-        Donation donation = new Donation(donator, donationStatus.pending, new Date(2024, 1, 1), new Date(2024, 1, 2));
+        Donation donation = new Donation(null, donationStatus.pending, new Date(2024, 1, 1), new Date(2024, 1, 2));
         donationsRepository.add(donation);
         Donation returnedDonation = donationsRepository.get(donation.getDonation_id());
         assertEquals(donation.getDonation_id(), returnedDonation.getDonation_id());
-        assertEquals(donation.getDonator().getUserId(), returnedDonation.getDonator().getUserId());
         assertEquals(donation.getStatus(), returnedDonation.getStatus());
         assertEquals(donation.getDonationDate(), returnedDonation.getDonationDate());
         assertEquals(donation.getAcceptDate(), returnedDonation.getAcceptDate());
     }
     @Test
     public void removeTest() {
-        Donation donation = new Donation(donator, donationStatus.pending, new Date(2024, 1, 1), new Date(2024, 1, 2));
+        Donation donation = new Donation(null, donationStatus.pending, new Date(2024, 1, 1), new Date(2024, 1, 2));
         donationsRepository.add(donation);
         Donation returnedDonation = donationsRepository.get(donation.getDonation_id());
         assertEquals(donation.getDonation_id(), returnedDonation.getDonation_id());
@@ -53,19 +38,18 @@ public class DonationsRepositoryTest {
     }
     @Test
     public void getTest() {
-        Donation donation = new Donation(donator, donationStatus.pending, new Date(2024, 1, 1), new Date(2024, 1, 2));
+        Donation donation = new Donation(null, donationStatus.pending, new Date(2024, 1, 1), new Date(2024, 1, 2));
         donationsRepository.add(donation);
         Donation returnedDonation = donationsRepository.get(donation.getDonation_id());
         assertEquals(donation.getDonation_id(), returnedDonation.getDonation_id());
-        assertEquals(donation.getDonator().getUserId(), returnedDonation.getDonator().getUserId());
         assertEquals(donation.getStatus(), returnedDonation.getStatus());
         assertEquals(donation.getDonationDate(), returnedDonation.getDonationDate());
         assertEquals(donation.getAcceptDate(), returnedDonation.getAcceptDate());
     }
     @Test
     public void getAllTest() {
-        Donation donation = new Donation(donator, donationStatus.pending, new Date(2024, 1, 1), new Date(2024, 1, 2));
-        Donation donation2 = new Donation(donator, donationStatus.pending, new Date(2024, 1, 1), new Date(2024, 1, 2));
+        Donation donation = new Donation(null, donationStatus.pending, new Date(2024, 1, 1), new Date(2024, 1, 2));
+        Donation donation2 = new Donation(null, donationStatus.pending, new Date(2024, 1, 1), new Date(2024, 1, 2));
         donationsRepository.add(donation);
         donationsRepository.add(donation2);
         assertEquals(2, donationsRepository.getAll().size());
