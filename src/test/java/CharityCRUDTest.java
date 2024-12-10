f0a89a6d563e33fe5a573463525fd0ea83e9a753
Kerelski <karoladamski22@wp.pl>
2024-12-10 19:00:36 +0100

    charityTest

--- src/test/java/CharityCRUDTest.java
+++ src/test/java/CharityCRUDTest.java
@@ -1,8 +1,8 @@
-package RepositoryTest;
-
 import Classes.Charity;
 import db.CharityRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,11 +10,19 @@ import org.junit.jupiter.api.Test;
 public class CharityCRUDTest {
 
     private CharityRepository repo;
+    private Charity charity;
+    @BeforeEach
+    public void setUp() {
+        charity = new Charity("zubr", "organizacja pomocowa od 1978 roku w Polsce");
+        repo = new CharityRepository();
+    }
+    @AfterEach
+    void cleanUp() {
+        repo.getAll().forEach(charity -> repo.remove(charity.getCharity_id()));
+    }
 
     @Test
     public void addTest() {
-        Charity charity = new Charity("zubr", "organizacja pomocowa od 1978 roku w Polsce");
-
 
         repo.add(charity);
         Charity returnedCharity = repo.get(charity.getCharity_id());
@@ -23,4 +31,5 @@ public class CharityCRUDTest {
         Assertions.assertEquals(charity.getCharity_description(), returnedCharity.getCharity_description());
         Assertions.assertEquals(charity.getCharity_name(), returnedCharity.getCharity_name());
     }
+
 }
